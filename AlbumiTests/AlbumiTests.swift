e76e275e5b59ddb13892c2030febc7e1230c150c
Mike <>
2020-01-31 14:13:04 +0800

    StoryBoard基本layout

---
+++ AlbumiTests/AlbumiTests.swift
@@ -0,0 +1,34 @@
+//
+//  AlbumiTests.swift
+//  AlbumiTests
+//
+//  Created by 陳昱宏 on 2020/1/5.
+//  Copyright © 2020 Mike. All rights reserved.
+//
+
+import XCTest
+@testable import Albumi
+
+class AlbumiTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
